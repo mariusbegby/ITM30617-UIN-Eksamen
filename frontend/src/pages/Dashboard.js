5e6e33faba49017448de76f0cb105a98e4b07712
Marius Begby <mariusbegby@gmail.com>
2023-04-25 13:26:49 +0200

    Designed dashboard component

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,11 +1,14 @@
 // Route: /
+import GameShopWidget from '../components/GameShopWidget';
+import MyGamesWidget from '../components/MyGamesWidget';
+import MyFavouritesWidget from '../components/MyFavouritesWidget';
+
 export default function Dashboard() {
     return (
-        <>
-            <main>
-                <h1>Dashboard Komponent</h1>
-            </main>
-            <aside></aside>
-        </>
+        <main id='dashboard-page'>
+            <GameShopWidget></GameShopWidget>
+            <MyGamesWidget></MyGamesWidget>
+            <MyFavouritesWidget></MyFavouritesWidget>
+        </main>
     );
 }
