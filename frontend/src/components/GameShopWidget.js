c12f6a84d9ef8ac1a5a6d7a554771232541597b3
Marius Begby <mariusbegby@gmail.com>
2023-04-25 16:29:39 +0200

    Replaced article elements for gamecard with GameCard component

--- frontend/src/components/GameShopWidget.js
+++ frontend/src/components/GameShopWidget.js
@@ -1,3 +1,13 @@
+import GameCard from './GameCard';
+
+const gameObject = {
+    title: 'Grand Theft Auto V',
+    slug: 'grand-theft-auto-v',
+    tags: ['Action', 'RPG'],
+    imageUrl: 'https://cdn.akamai.steamstatic.com/steam/apps/271590/header.jpg?t=1678296348',
+    bought: false
+};
+
 export default function GameShopWidget() {
     return (
         <section id='gameshop-widget'>
@@ -8,52 +18,9 @@ export default function GameShopWidget() {
                 </a>
             </header>
             <div id='gameshop-widget-gameslist' className='gameslist'>
-                <article>
-                    <figure>
-                        <img src='https://cdn.akamai.steamstatic.com/steam/apps/730/header.jpg?t=1668125812' alt='' />
-                    </figure>
-                    <section>
-                        <h3>Counter-Strike: Global Offensive</h3>
-                        <div className="tag-section">
-                            <p>Action</p>
-                            <p>RPG</p>
-                        </div>
-                        <a className='link-button' href='/'>
-                            Buy
-                        </a>
-                    </section>
-                </article>
-                <article>
-                    <figure>
-                        <img src='https://cdn.akamai.steamstatic.com/steam/apps/1774580/header.jpg?t=1681406818' alt='' />
-                    </figure>
-                    <section>
-                        <h3>STAR WARS Jedi: Survivor&trade;</h3>
-                        <div className="tag-section">
-                            <p>Action</p>
-                            <p>Adventure</p>
-                        </div>
-                        <a className='link-button' href='/'>
-                            Buy
-                        </a>
-                    </section>
-                </article>
-                <article>
-                    <figure>
-                        <img src='https://cdn.akamai.steamstatic.com/steam/apps/1091500/header.jpg?t=1680026109' alt='' />
-                    </figure>
-                    <section>
-                        <h3>Cyberpunk 2077</h3>
-                        <div className="tag-section">
-                            <p>Cyberpunk</p>
-                            <p>RPG</p>
-                            <p>Action</p>
-                        </div>
-                        <a className='link-button' href='/'>
-                            Buy
-                        </a>
-                    </section>
-                </article>
+                <GameCard key='1' gameObject={gameObject}></GameCard>
+                <GameCard key='2' gameObject={gameObject}></GameCard>
+                <GameCard key='3' gameObject={gameObject}></GameCard>
             </div>
         </section>
     );
