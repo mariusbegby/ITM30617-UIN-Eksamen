99bbb4544ab36a8749395dce68a70292eae6e9ec
Marius Begby <mariusbegby@gmail.com>
2023-04-27 13:34:22 +0200

    Only show mygames and favourites widgets when logged in

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,14 +1,17 @@
 // Route: /
+import React, { useContext } from 'react';
 import GameShopWidget from '../components/GameShopWidget';
 import MyGamesWidget from '../components/MyGamesWidget';
 import MyFavouritesWidget from '../components/MyFavouritesWidget';
+import { LoginContext } from '../contexts/LoginContext';
 
 export default function Dashboard() {
+    const { loggedInUser } = useContext(LoginContext);
     return (
         <main id='dashboard-page'>
             <GameShopWidget></GameShopWidget>
-            <MyGamesWidget></MyGamesWidget>
-            <MyFavouritesWidget></MyFavouritesWidget>
+            {loggedInUser && <MyGamesWidget loggedInUser={loggedInUser}></MyGamesWidget>}
+            {loggedInUser && <MyFavouritesWidget loggedInUser={loggedInUser}></MyFavouritesWidget>}
         </main>
     );
 }
