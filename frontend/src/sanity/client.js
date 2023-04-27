42fe81e16feebe5a47eb5a19ecc34cebb6f5a91f
Marius Begby <mariusbegby@gmail.com>
2023-04-27 13:32:06 +0200

    Update Sanity client with write permission

--- frontend/src/sanity/client.js
+++ frontend/src/sanity/client.js
@@ -4,7 +4,9 @@ const client = createClient({
     projectId: 'toiktntw',
     dataset: 'production',
     apiVersion: '2021-10-21',
-    useCdn: true
+    token: 'skZTsrbfdsmChMXW1Jc5WixaNhixbyy4ItkbUdJtVkqVH9xkcaYFE88gp15BQmWb5oENN9iwBaxLtq8bsgsquUCoA7dIlX7Lb6M9eygmfW9BHlaEhh9EFf5vGuk2c9m7qhaXnhoeODTGdSpANQHVbfiwfnW7wUDchpq8DJh2OZmDjSDDkKH7',
+    useCdn: false,
+    ignoreBrowserTokenWarning: true
 });
 
 export default client;
