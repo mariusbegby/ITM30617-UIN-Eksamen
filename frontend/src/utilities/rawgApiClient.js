6656e113a46f2d89c6c6240af0c34666bf6499f1
Marius Begby <mariusbegby@gmail.com>
2023-04-28 22:32:29 +0200

    Refactor RAWG.io API client

--- frontend/src/utilities/rawgApiClient.js
+++ frontend/src/utilities/rawgApiClient.js
@@ -1,18 +1,21 @@
 import { apiKey } from '../apiKey';
 
-export const fetchGameInfo = async (gameSlugOrId) => {
-    const response = await fetch(
-        'https://rawg.io/api/games/' + gameSlugOrId + '?key=' + apiKey
-    );
+const apiBaseURL = 'https://rawg.io/api';
+
+const fetchData = async (endpoint) => {
+    const response = await fetch(endpoint + `?key=${apiKey}`);
     const data = await response.json();
     return data;
 };
 
+export const fetchGameInfo = async (gameSlugOrId) => {
+    const endpoint = `${apiBaseURL}/games/${gameSlugOrId}`;
+    return fetchData(endpoint);
+};
+
 export const fetchSteamUrlForGame = async (gameSlugOrId) => {
-    const response = await fetch(
-        'https://rawg.io/api/games/' + gameSlugOrId + '/stores?key=' + apiKey
-    );
-    const data = await response.json();
+    const endpoint = `${apiBaseURL}/games/${gameSlugOrId}/stores`;
+    const data = await fetchData(endpoint);
     const steamStoreObject = data.results.find((store) => store.store_id === 1);
     const steamUrl = steamStoreObject.url;
     return steamUrl ? steamUrl : null;
