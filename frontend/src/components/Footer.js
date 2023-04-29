1e92f4c11a63df7d147dc93fec7834d0918ae0b2
Marius Begby <mariusbegby@gmail.com>
2023-04-29 02:36:53 +0200

    Remove unnecessary section tag from footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,11 @@
 export default function Footer() {
     return (
         <footer>
-            <section>
-                <p>
-                    Made possible using{' '}
-                    <a href='https://rawg.io/apidocs'>RAWG.io API</a> for video
-                    game data and images.
-                </p>
-            </section>
+            <p>
+                Made possible using{' '}
+                <a href='https://rawg.io/apidocs'>RAWG.io API</a> for video game
+                data and images.
+            </p>
         </footer>
     );
 }
